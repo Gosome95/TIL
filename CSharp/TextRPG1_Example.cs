c6edb37f0eb3d8f8c4b5fcb977585ef9d9b0af64
KimMorgan <sagep@naver.com>
2022-11-16 16:23:13 +0900

    UPDATE Example1 - use : enum type

---
+++ CSharp/TextRPG1_Example.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        //Add enum
+        enum ClassType
+        {
+            None = 0,
+            Knight = 1,
+            Archer = 2,
+            Mage = 3
+         }
+     
+        static void Main(string[] args)
+        {
+            // init(initialization) enum
+            ClassType choice = ClassType.None; 
+
+            while (true)
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("직업을 선택하세요!");
+                Console.WriteLine("[1] 기사");
+                Console.WriteLine("[2] 궁수");
+                Console.WriteLine("[3] 법사");
+
+                string input = Console.ReadLine();
+
+                switch(input)
+                {
+                    case "1":
+                        choice = ClassType.Knight;
+                        break;
+                    case "2":
+                        choice = ClassType.Archer;
+                        break;
+                    case "3":
+                        choice = ClassType.Mage;
+                        break; 
+                }
+                if (choice != ClassType.None)
+                    break;
+
+            }
+
+        }
+    }
+}
