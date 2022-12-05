f56144bae041b745b40d17f12a1c0f7851a4ff4c
KimMorgan <sagep@naver.com>
2022-12-05 19:54:22 +0900

    REVIEW [TextRPG2] Player class _Practice

--- CSharp/TextRPG2_OOP/TextRPG2_Example/Player.cs
+++ CSharp/TextRPG2_OOP/TextRPG2_Example/Player.cs
@@ -14,50 +14,51 @@ namespace TextRPG2_Example
     }
     class Player
     {
+        protected int _hp = 0;
+        protected int _attack = 0;
         protected PlayerType _type = PlayerType.None;
-        protected int _hp;
-        protected int _attack;
+
         protected Player(PlayerType type)
         {
             _type = type;
         }
 
-        public void SetInfo(int hp, int attack)
+        protected void SetInfo(int hp, int attack)
         {
             _hp = hp;
             _attack = attack;
         }
-        public PlayerType GetPlayerType() { return _type; }
-        public int GetHP() { return _hp; }
-        public int GetAttack() { return _attack; }
+        public PlayerType GetPlayerType(){ return _type; }
+        public int GetHP(){ return _hp; }
+        public int GetAttack(){ return _attack; }
         public bool IsDead() { return _hp <= 0; }
-        public void OnDamaged(int damage)
+        public void OndDamaged(int damage)
         {
             _hp -= damage;
-            if(_hp < 0)
+            if (_hp < 0)
                 _hp = 0;
         }
     }
 
     class Knight : Player
     {
-        public Knight() :base(PlayerType.Knight)
+        public Knight() : base(PlayerType.Knight)
         {
             SetInfo(100, 10);
         }
     }
-     class Archer : Player
+    class Archer : Player
     {
-        public Archer() :base(PlayerType.Archer)
+        public Archer() : base(PlayerType.Archer)
         {
-            SetInfo(75, 12);
+            SetInfo(75, 12);            
         }
     }
-     class Mage : Player
+    class Mage : Player
     {
-        public Mage() :base(PlayerType.Mage)
+        public Mage() : base(PlayerType.Mage)
         {
             SetInfo(60, 15);
         }
-    }                  
+    }              
 }
