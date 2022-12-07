eddc8cb19339eeafcee5139b116b717b5e17acee
KimMorgan <sagep@naver.com>
2022-12-07 22:22:45 +0900

    UPDATE [TextRPG2] ProcessField & Fight & Escape method Example

--- CSharp/TextRPG2_OOP/TextRPG2_Example/Game.cs
+++ CSharp/TextRPG2_OOP/TextRPG2_Example/Game.cs
@@ -17,6 +17,8 @@ namespace TextRPG2_Example
     {
         protected GameMode mode = GameMode.Lobby;
         private Player player = null;
+        private Monster monster = null;
+        Random rand = new Random();
 
         public void Process()
         {
@@ -29,6 +31,7 @@ namespace TextRPG2_Example
                     ProcessTown();
                     break;
                 case GameMode.Field:
+                    ProcessField();
                     break;
             }
         }
@@ -73,6 +76,77 @@ namespace TextRPG2_Example
                     mode = GameMode.Lobby;
                     break;
             }
+        }
+        private void ProcessField()
+        {
+            Console.WriteLine("필드에 입장했습니다");
+            CreateRandmMonster();
+
+            Console.WriteLine("[1] 싸우기");
+            Console.WriteLine("[2] 일정확률로 도망가기");
+            string input = Console.ReadLine();
+            switch(input)
+            {
+                case "1":
+                    ProcessFight();
+                    break;
+                case "2":
+                    TryEscape();
+                    break;
+            }
+        }
+        private void CreateRandmMonster()
+        {
+            int randValue = rand.Next(0, 3);
+            switch(randValue)
+            {
+                case 0:
+                    monster = new Slime();
+                    System.Console.WriteLine("슬라임이 생성되었습니다");
+                    break;
+                case 1:
+                    monster = new Orc();
+                    System.Console.WriteLine("슬라임이 생성되었습니다");
+                    break;
+                case 2:
+                    monster = new Skeleton();
+                    System.Console.WriteLine("슬라임이 생성되었습니다");
+                    break;
+            }
+        }
+        private void ProcessFight()
+        {
+            while(true)
+            {
+                int damage = player.GetAttack();
+                monster.OnDamaged(damage);
+                if(monster.IsDead())
+                {
+                    System.Console.WriteLine("승리했습니다");
+                    System.Console.WriteLine($"남은 체력 {player.GetHP()}");
+                    break;
+                }
+
+                damage = monster.GetAttack();
+                player.OnDamaged(damage);
+                if(player.IsDead())
+                {
+                    System.Console.WriteLine("패배했습니다");
+                    System.Console.WriteLine("로비로 돌아갑니다");
+                    mode = GameMode.Lobby;
+                    break;
+                }
+            }
+        }
+        private void TryEscape()
+        {
+            int randValue = rand.Next(1, 101);
+            if(randValue < 33)
+            {
+                mode = GameMode.Town;
+            }
+            else
+                ProcessFight();
         }
     }
 }
