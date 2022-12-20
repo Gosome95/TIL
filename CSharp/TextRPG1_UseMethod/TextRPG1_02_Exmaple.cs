33d2a20e8c212b8bdbd2a8ff6c3108faaa29d266
KimMorgan <sagep@naver.com>
2022-12-20 20:01:35 +0900

    REVIEW [TextRPG1] daily pracitce_5

--- CSharp/TextRPG1_UseMethod/TextRPG1_02_Exmaple.cs
+++ CSharp/TextRPG1_UseMethod/TextRPG1_02_Exmaple.cs
@@ -4,7 +4,7 @@ namespace MyApp
 {
     class Program
     {
-        enum ClassType
+        enum PlayerType
         {
             None = 0,
             Knight = 1,
@@ -16,12 +16,13 @@ namespace MyApp
             public int hp;
             public int attack;
         }
+
         enum MonsterType
         {
-            None,
-            Slime,
-            Orc,
-            Skeleton
+            None = 0,
+            Slime = 1,
+            Orc = 2,
+            Skeletone = 3
         }
         struct Monster
         {
@@ -29,74 +30,78 @@ namespace MyApp
             public int attack;
         }
 
-        static ClassType ChooseClass()
+        static PlayerType ChoooseClass()
         {
-            ClassType choice = ClassType.None;
-            System.Console.WriteLine("게임에 접속했습니다");
-            System.Console.WriteLine("[1] 기사");
-            System.Console.WriteLine("[2] 궁수");
-            System.Console.WriteLine("[3] 마법사");
-            System.Console.Write(" : ");
+            PlayerType choice = PlayerType.None;
+            Console.WriteLine("게임에 접속했습니다");
+            Console.WriteLine("[1] 기사");
+            Console.WriteLine("[2] 궁수");
+            Console.WriteLine("[2] 마법사");
+            Console.WriteLine("직업을 선택해주세요");
+            Console.Write(" : ");
             string input = Console.ReadLine();
-            switch (input)
+
+            switch(input)
             {
                 case "1":
-                    choice = ClassType.Knight;
-                    System.Console.WriteLine("기사 선택");
+                    choice = PlayerType.Knight;
                     break;
                 case "2":
-                    choice = ClassType.Archer;
-                    System.Console.WriteLine("궁수 선택");
+                    choice = PlayerType.Archer;
                     break;
                 case "3":
-                    choice = ClassType.Mage;
-                    System.Console.WriteLine("마법사 선택");
+                    choice = PlayerType.Mage;
                     break;
             }
             return choice;
         }
-        static void CreatePlayer(ClassType choice, out Player player)
+        static void CreatePlayer(PlayerType choice, out Player player)
         {
-            switch (choice)
+            switch(choice)
             {
-                case ClassType.Knight:
+                case PlayerType.Knight:
                     player.hp = 100;
                     player.attack = 10;
+                    Console.WriteLine("당신의 직업은 [기사] 입니다");
                     break;
-                case ClassType.Archer:
-                    player.hp = 100;
-                    player.attack = 10;
+                case PlayerType.Archer:
+                    player.hp = 70;
+                    player.attack = 12;
+                    Console.WriteLine("당신의 직업은 [궁수] 입니다");
                     break;
-                case ClassType.Mage:
-                    player.hp = 100;
-                    player.attack = 10;
+                case PlayerType.Mage:
+                    player.hp = 60;
+                    player.attack = 18;
+                    Console.WriteLine("당신의 직업은 [마법사] 입니다");
                     break;
                 default:
                     player.hp = 0;
                     player.attack = 0;
                     break;
             }
+            Console.WriteLine($"현재 능력치 체력: {player.hp} | 공격력: {player.attack}");
         }
-        static void CreateRandomMonster(out Monster monster)
+
+        static void CreateMonster(out Monster monster)
         {
             Random rand = new Random();
-            MonsterType randMon = (MonsterType)rand.Next(1, 4);
-            switch (randMon)
+            MonsterType monsterType = (MonsterType)rand.Next(1, 4);
+            switch(monsterType)
             {
                 case MonsterType.Slime:
                     monster.hp = 10;
-                    monster.attack = 10;
-                    System.Console.WriteLine("슬라임이 리젠되었습니다");
+                    monster.attack = 3;
+                    Console.WriteLine("슬라임이 리젠되었습니다");
                     break;
                 case MonsterType.Orc:
-                    monster.hp = 20;
-                    monster.attack = 20;
-                    System.Console.WriteLine("오크가 리젠되었습니다");
-                    break;
-                case MonsterType.Skeleton:
                     monster.hp = 15;
-                    monster.attack = 30;
-                    System.Console.WriteLine("스켈레톤이 리젠되었습니다");
+                    monster.attack = 5;
+                    Console.WriteLine("오크가 리젠되었습니다");
+                    break;
+                case MonsterType.Skeletone:
+                    monster.hp = 13;
+                    monster.attack = 7;
+                    Console.WriteLine("스켈레톤이 리젠되었습니다");
                     break;
                 default:
                     monster.hp = 0;
@@ -110,82 +115,64 @@ namespace MyApp
             while (true)
             {
                 monster.hp -= player.attack;
+                Console.WriteLine($"{player.attack} 데미지를 입혔습니다");
                 if (monster.hp <= 0)
                 {
-                    System.Console.WriteLine("승리하였습니다");
-                    Console.WriteLine($"남은 체력{player.hp}");
+                    Console.WriteLine("승리했습니다");
+                    Console.WriteLine($"현재 체력{player.hp}");
                     break;
                 }
 
                 player.hp -= monster.attack;
-                if (player.hp <= 0)
+                if(player.hp <= 0)
                 {
-                    System.Console.WriteLine("패배하였습니다");
+                    Console.WriteLine("패배했습니다");
                     break;
                 }
             }
         }
+
         static void EnterField(ref Player player)
         {
-            System.Console.WriteLine("필드에 들어왔습니다");
-            while (true)
+            Console.WriteLine("필드에 들어왔습니다");
+            while(true)
             {
                 Monster monster;
-                CreateRandomMonster(out monster);
-                System.Console.WriteLine("[1] 싸운다");
-                System.Console.WriteLine("[2] 도망간다");
+                CreateMonster(out monster);
+                Console.WriteLine("[1] 싸운다");
+                Console.WriteLine("[2] 도망간다");
+                Console.Write(" : ");
                 string input = Console.ReadLine();
-                if (input == "1")
+                if(input == "1")
                 {
                     Fight(ref player, ref monster);
-                    if (player.hp < 50)
-                    {
-                        Console.WriteLine("계속 사냥하시겠습니까");
-                        System.Console.WriteLine("[1] 계속 사냥");
-                        System.Console.WriteLine("[2] 마을로 돌아간다");
-                        string continueHunt = Console.ReadLine();
-                        if (continueHunt == "1")
-                            continue;
-                        else if (continueHunt == "2")
-                            break;
-                    }
                 }
-                else if (input == "2")
+                else if(input == "2")
                 {
                     Random rand = new Random();
                     int randValue = rand.Next(1, 101);
-                    if (randValue <= 33)
+                    if(randValue <= 33)
                     {
-                        System.Console.WriteLine("마을로 돌아갑니다");
-                        System.Console.WriteLine("--------------------");
+                        Console.WriteLine("마을로 돌아갑니다");
                         break;
                     }
                     else
                     {
                         Console.WriteLine("도망치지 못했습니다");
+                        Console.WriteLine();
                         Fight(ref player, ref monster);
-                        if (player.hp < 50)
-                        {
-                            Console.WriteLine("계속 사냥하시겠습니까");
-                            System.Console.WriteLine("[1] 계속 사냥");
-                            System.Console.WriteLine("[2] 마을로 돌아간다");
-                            string continueHunt = Console.ReadLine();
-                            if (continueHunt == "1")
-                                continue;
-                            else if (continueHunt == "2")
-                                break;
-                        }
                     }
                 }
             }
         }
-        static void EnterGame(ref Player player)
+
+        static void EnterTown(ref Player player)
         {
-            while (true)
+            while(true)
             {
-                System.Console.WriteLine("마을에 입장했습니다");
-                System.Console.WriteLine("[1] 필드로 가기");
-                System.Console.WriteLine("[2] 로비로 돌아가기");
+                Console.WriteLine("필드에 들어왔습니다");
+                Console.WriteLine("[1] 필드로 간다");
+                Console.WriteLine("[2] 로비로 돌아간다");
                 Console.Write(" : ");
                 string input = Console.ReadLine();
 
@@ -195,8 +182,8 @@ namespace MyApp
                 }
                 else if (input == "2")
                 {
-                    System.Console.WriteLine("로비로 돌아갑니다");
-                    System.Console.WriteLine("--------------------");
+                    Console.WriteLine("로비로 돌아갑니다");
+                    Console.WriteLine("-------------------");
                     break;
                 }
             }
@@ -204,15 +191,15 @@ namespace MyApp
 
         static void Main(string[] args)
         {
-            while (true)
+            while(true)
             {
-                ClassType choice = ChooseClass();
-                if (choice == ClassType.None)
+                PlayerType choice = ChoooseClass();
+                if (choice == PlayerType.None)
                     continue;
 
                 Player player;
                 CreatePlayer(choice, out player);
-                EnterGame(ref player);
+                EnterTown(ref player);
             }
         }
     }
